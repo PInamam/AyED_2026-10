3df01c3ea1b74c8aafd65e7082d23da67a4b5db1
PInamam <pablomamani200989@gmail.com>
2026-03-26 16:16:48 -0300

    Add files via upload

---
+++ TP1 26-3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pablo_Mamani_4__2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string pelicula;
+
+            Console.WriteLine("Hola usuario");
+            Console.Write("¿Cual es su nombre?: ");
+            nombre = Console.ReadLine();
+            Console.Write("¿Cual es tu pelicula favorita?: ");
+            pelicula = Console.ReadLine();
+
+            Console.WriteLine("Hola " + nombre + " .Tu pelicula preferida " + pelicula + " es una porqueria");
+
+
+        }
+    }
+}
