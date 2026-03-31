68364ad90d0bc5d552a632376641825cd7fdc41c
PInamam <pablomamani200989@gmail.com>
2026-03-31 14:09:38 -0300

    Add files via upload

---
+++ TP3 31-3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int año;
+            int edad;
+
+            Console.WriteLine("Hola usuario");
+            Console.Write("¿Cual es su año de nacimiento?: ");
+            año = Convert.ToInt32(Console.ReadLine());
+            edad = 2026 - año;
+
+            Console.WriteLine("Si naciste en el año " + año + " tu edad al comenzar el año es de " + edad + " años.");
+        }
+    }
+}
