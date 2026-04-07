2c27e608970eeb7032d89a49656b3bb04ff89653
PInamam <pablomamani200989@gmail.com>
2026-04-07 15:55:57 -0300

    Add files via upload

---
+++ TP5 7-4.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int costo_minuto, costo_hora;
+            costo_minuto = 150;
+            costo_hora = 6000;
+
+            int tiempo, horas, minutos;
+
+            Console.Write("Ingrese los minutos que su auto estuvo estacionado: ");
+            tiempo = Convert.ToInt32(Console.ReadLine());
+            horas = tiempo / 60;
+            minutos = tiempo % 60;
+            Console.WriteLine("Horas: " + horas);
+            Console.WriteLine("Minutos: " + minutos);
+
+
+            double total, total_a_pagar, iva;
+            total = horas * costo_hora + minutos * costo_minuto;
+            iva = total * 21 / 100;
+            total_a_pagar = total + iva;
+
+            Console.WriteLine("Monto: " + total);
+            Console.WriteLine("Monto + IVA: " + total_a_pagar);
+
+        }
+    }
+}
