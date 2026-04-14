ce9a0c9c3a1232d527cf8abd137f8ba8f59f745a
PInamam <pablomamani200989@gmail.com>
2026-04-14 16:55:14 -0300

    Add files via upload

---
+++ Condicionales - Primera parte/7. Clima de Montaña.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Ingrese la temperatura actual: ");
+            double temperatura = Convert.ToDouble(Console.ReadLine());
+
+            if (temperatura > 15)
+            {
+                Console.WriteLine("Clima agradable.");
+            }
+            else if (temperatura >= 0)
+            {
+                Console.WriteLine("Mucho frio.");
+            }
+            else
+            {
+                Console.WriteLine("Peligro de congelamiento.");
+            }
+        }
+    }
+}
