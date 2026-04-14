71e7300633eb0a1bb7525ebcf3110e17fc369a21
PInamam <pablomamani200989@gmail.com>
2026-04-14 16:09:34 -0300

    Add files via upload

---
+++ Condicionales - Primera parte/2. El Club de la Pelea (Ingreso).cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Ingrese un numero entero: ");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero%2 == 0)
+            {
+                Console.WriteLine("El numero " + numero + " es par.");
+            }
+            else
+            {
+                Console.WriteLine("El numero " + numero + " no es par.");
+            }
+        }
+    }
+}
