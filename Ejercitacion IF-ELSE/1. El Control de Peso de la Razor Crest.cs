2e27c42e7bb0e6a168489bec958969c55d96f51b
PInamam <pablomamani200989@gmail.com>
2026-04-14 14:24:34 -0300

    Add files via upload

---
+++ Ejercitacion IF-ELSE/1. El Control de Peso de la Razor Crest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double peso, sobrePeso;
+            Console.Write("Ingrese el peso de la carga actual: ");
+            peso = Convert.ToDouble(Console.ReadLine());
+
+            if (peso <= 5000)
+            {
+                Console.WriteLine("Despegue autorizado. Buen viaje, Mando");
+            }
+            else {
+                sobrePeso = peso - 5000;
+                Console.WriteLine("Alerta: Exceso de peso. Debes descargar " + sobrePeso + "kg para despegar");
+            }
+            
+
+        }
+    }
+}
