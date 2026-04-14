6d5eba0d4e0e6904b0b2a4b928168bd12e88cda8
PInamam <pablomamani200989@gmail.com>
2026-04-14 14:33:40 -0300

    Add files via upload

---
+++ Ejercitacion IF-ELSE/2. Clasificador de Aspirantes a la Academia Jedi.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int midiclorianos;
+            Console.Write("ingrese la cantidad de midiclorianos detectados en el sujeto: ");
+            midiclorianos = Convert.ToInt32(Console.ReadLine());
+
+            if (midiclorianos > 15000)
+            {
+                Console.WriteLine("Potencial Maestro Jedi detectado. Avisar al Consejo.");
+            }
+            else if (midiclorianos >= 5000)
+            {
+                Console.WriteLine("Aceptable para entrenamiento Padawan.");
+            }
+            else
+            {
+                Console.WriteLine("No posee sensibilidad a la Fuerza.");
+            }
+            
+
+        }
+    }
+}
