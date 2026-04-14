ca6e04f80eeb923d6ab695715efea022ad04f4da
PInamam <pablomamani200989@gmail.com>
2026-04-14 16:54:53 -0300

    Add files via upload

---
+++ Condicionales - Primera parte/6. Login Simple.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Nombre de usuario: ");
+            string usuario = Console.ReadLine();
+
+            Console.Write("Contraseña: ");
+            string contraseña = Console.ReadLine();
+
+            if (usuario == "admin" && contraseña == "1234")
+            {
+                Console.WriteLine("Acceso concedido.");
+            }
+            else
+            {
+                Console.WriteLine("Error de credenciales.");
+            }
+
+        }
+    }
+}
