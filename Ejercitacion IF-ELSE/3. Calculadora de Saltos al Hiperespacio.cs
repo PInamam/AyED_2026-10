18e7e7ec791958f2dcac5416aab9c83ad6517e2a
PInamam <pablomamani200989@gmail.com>
2026-04-14 15:30:32 -0300

    Add files via upload

---
+++ Ejercitacion IF-ELSE/3. Calculadora de Saltos al Hiperespacio.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int droides, precioDroide;
+
+            precioDroide = 2000;
+            Console.Write("Ingrese la cantidad de droides a comprar: ");
+            droides = Convert.ToInt32(Console.ReadLine());
+
+            int totalOriginal;
+            totalOriginal = precioDroide * droides;
+
+            if (droides >= 3)
+            {
+                double descuento = (20 * totalOriginal) / 100;
+                double precioFinal = totalOriginal - descuento;
+
+                Console.WriteLine("Total original: $" + totalOriginal);
+                Console.WriteLine("Descuento: $" + descuento);
+                Console.WriteLine("Precio final: $" + precioFinal);
+            }
+            else
+            {
+                Console.WriteLine("Total: $" + totalOriginal);
+            }
+
+        }
+    }
+}
