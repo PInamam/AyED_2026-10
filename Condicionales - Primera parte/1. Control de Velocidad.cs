57669264a406e997d1f3bc510ed60ce160714ffb
PInamam <pablomamani200989@gmail.com>
2026-04-23 17:06:02 -0300

    Add files via upload

---
+++ Condicionales - Primera parte/1. Control de Velocidad.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Ingrese la velocidad de un vehiculo: ");
+            double velocidadVehiculo = Convert.ToDouble(Console.ReadLine());
+
+            if (velocidadVehiculo > 120)
+            {
+                Console.WriteLine("Multa en camino: Exceso de velocidad.");
+            }
+            else
+            {
+                Console.WriteLine("Conducción segura.");
+            }
+
+        }
+    }
+}
