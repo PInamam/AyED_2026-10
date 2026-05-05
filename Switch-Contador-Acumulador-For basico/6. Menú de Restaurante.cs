c621cba24ca78b2769ab43978b1d4aed841a0e3c
PInamam <pablomamani200989@gmail.com>
2026-05-05 16:26:45 -0300

    Add files via upload

---
+++ Switch-Contador-Acumulador-For basico/6. Menú de Restaurante.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("MENU: ");
+            Console.WriteLine("(S)ándwich");
+            Console.WriteLine("(P)izza");
+            Console.WriteLine("(H)amburguesa");
+            Console.Write("Ingrese una opcion del menu: ");
+            string opcion = Console.ReadLine();
+
+            switch (opcion)
+            {
+                case "P":
+                    Console.WriteLine("Precio de la Pizza: $10000");
+                    break;
+
+                case "S":
+                    Console.WriteLine("Precio del Sandwhich: $6000");
+                    break;
+
+                case "H":
+                    Console.WriteLine("Precio de la Hamburguesa: $8000");
+                    break;
+
+                default:
+                    Console.WriteLine("Producto no encontrado");
+                    break;
+            }
+
+
+        }
+    }
+}
---
+++ Switch-Contador-Acumulador-For basico/7. Selector de idioma.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("IDIOMAS: ");
+            Console.WriteLine("1: Inglés");
+            Console.WriteLine("2: Francés");
+            Console.WriteLine("3: Alemán");
+            Console.Write("Ingrese una opcion de idioma: ");
+            string opcion = Console.ReadLine();
+
+            switch (opcion)
+            {
+                case "1":
+                    Console.WriteLine("Hello");
+                    break;
+
+                case "2":
+                    Console.WriteLine("Salut");
+                    break;
+
+                case "3":
+                    Console.WriteLine("Hallo");
+                    break;
+
+                default:
+                    Console.WriteLine("Idioma no disponible");
+                    break;
+            }
+
+
+        }
+    }
+}
