e8b8ae2b26c42e0d05d706b86278f49d0336f09e
PInamam <pablomamani200989@gmail.com>
2026-05-26 17:01:07 -0300

    Add files via upload

---
+++ Guia While - Do while #2/Ejercicio 5 Contador de números positivos, negativos y ceros.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string ingreso;
+            int numeroIngresado;
+            bool detenerBucle = false;
+            int contadorPositivos = 0;
+            int contadorCeros = 0;
+            int contadorNegativos = 0;
+
+            while (detenerBucle == false)
+            {
+                Console.Write("Numero: ");
+                ingreso = Console.ReadLine();
+
+                if (ingreso == "r" || ingreso == "R" || ingreso == "n" || ingreso == "N")
+                {
+                    detenerBucle = true;
+                }
+                else
+                {
+                    numeroIngresado = Convert.ToInt32(ingreso);
+
+                    if (numeroIngresado > 0)
+                    {
+                        contadorPositivos++;
+                    }
+                    else if(numeroIngresado == 0)
+                    {
+                        contadorCeros++;
+                    }
+                    else
+                    {
+                        contadorNegativos++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Positivos: " + contadorPositivos);
+            Console.WriteLine("Ceros: " + contadorCeros);
+            Console.WriteLine("Negativos: " + contadorNegativos);
+
+        }
+    }
+}
