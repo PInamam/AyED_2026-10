809e1b0f55735b1a8998b78ca639a6efe8ab7b86
PInamam <pablomamani200989@gmail.com>
2026-05-26 15:41:43 -0300

    Add files via upload

---
+++ Guia While - Do while #2/Ejercicio 4 Sistema de login con intentos bloqueados.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string nombreUsuarioRegistrado = "Usuario123";
+            string contraseñaRegistrada = "1234";
+
+            string nombreUsuarioIngresado;
+            string contraseñaIngresada;
+
+            bool datosCorrectos = false;
+
+            int intentos = 0;
+
+            while (intentos < 3 && datosCorrectos == false)
+            {
+                Console.WriteLine("");
+                Console.Write("Nombre de Usuario: ");
+                nombreUsuarioIngresado = Console.ReadLine();
+                Console.Write("Contraseña: ");
+                contraseñaIngresada = Console.ReadLine();
+
+                intentos++;
+
+                if (nombreUsuarioIngresado == nombreUsuarioRegistrado && contraseñaIngresada == contraseñaRegistrada)
+                {
+                    datosCorrectos = true;
+                    Console.WriteLine("Bienvenido al sistema");
+                }
+            }
+
+            if (intentos >= 3)
+            {
+                Console.WriteLine("Cuenta bloqueada por seguridad");
+            }
+
+        }
+    }
+}
