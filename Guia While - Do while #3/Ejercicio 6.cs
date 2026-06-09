80c8d842c01a5a39208f49c3093c20af9d2f2c61
PInamam <pablomamani200989@gmail.com>
2026-06-09 15:54:09 -0300

    Add files via upload

---
+++ Guia While - Do while #3/Ejercicio 6.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double precioProducto = 1;
+            double montoAcumulado = 0;
+
+            while (precioProducto != 0)
+            {
+                Console.Write("Precio producto: $");
+                precioProducto = Convert.ToDouble(Console.ReadLine());
+                montoAcumulado = montoAcumulado + precioProducto;
+            }
+
+            if (montoAcumulado > 15000)
+            {
+                double descuento = montoAcumulado * 0.10;
+                double montoFinal = montoAcumulado - descuento;
+                Console.WriteLine("Subtotal: $" + montoAcumulado);
+                Console.WriteLine("Descuento aplicado: $" + descuento);
+                Console.WriteLine("Total final a pagar: $" + montoFinal);
+            }
+            else
+            {
+                Console.WriteLine("Total final a pagar: $" + montoAcumulado);
+            }
+   
+        }
+    }
+}
---
+++ Guia While - Do while #3/Ejercicio 7.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombreAlumno;
+            string estadoAlumno;
+            string opcion = "";
+            int cantPresentes = 0;
+            int cantAusentes = 0;
+            int totalAlumnos = 0;
+
+            do
+            {
+                Console.Write("Nombre alumno: ");
+                nombreAlumno = Console.ReadLine();
+                do
+                {
+                    Console.Write("Estado (P/A): ");
+                    estadoAlumno = Console.ReadLine();
+                }
+                while (estadoAlumno != "P" && estadoAlumno != "A");
+                totalAlumnos++;
+
+                if (estadoAlumno == "P")
+                {
+                    cantPresentes++;
+                }
+                else
+                {
+                    cantAusentes++;
+                }
+
+                do
+                {
+                    Console.Write("Desea cargar otro alumno?(S/N): ");
+                    opcion = Console.ReadLine();
+                }
+                while (opcion != "S" && opcion != "N");
+            }
+            while (opcion == "S");
+
+
+            double porcentajePresentismo = cantPresentes * 100 / totalAlumnos;
+
+            Console.WriteLine("");
+            Console.WriteLine("Total alumnos: " + totalAlumnos);
+            Console.WriteLine("Presentes: " + cantPresentes);
+            Console.WriteLine("Ausentes: " + cantAusentes);
+            Console.WriteLine("Presentismo: " + porcentajePresentismo + "%");
+
+        }
+    }
+}
