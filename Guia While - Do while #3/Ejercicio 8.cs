8d0479e44b340eba2c17366b6e08676bfa4db34f
PInamam <pablomamani200989@gmail.com>
2026-06-09 17:00:04 -0300

    Add files via upload

---
+++ Guia While - Do while #3/Ejercicio 8.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int edad = -1;
+            int personasRegistradas = -1;
+            int sumaDeEdades = 0;
+            int menoresDeEdad = 0;
+            int mayoresDeEdad = 0;
+
+            do
+            {
+                personasRegistradas++;
+                sumaDeEdades = sumaDeEdades + edad;
+                if (edad >= 18)
+                {
+                    mayoresDeEdad++;
+                }
+                else if (edad >= 0)
+                {
+                    menoresDeEdad++;
+                }
+                Console.Write("Ingrese edad: ");
+                edad = Convert.ToInt32(Console.ReadLine());
+            }
+            while (edad >= 0);
+
+            double promedioEdades = sumaDeEdades / personasRegistradas;
+
+            Console.WriteLine("");
+            Console.WriteLine("Personas registradas: " + personasRegistradas);
+            Console.WriteLine("Promedio de edad: " + promedioEdades);
+            Console.WriteLine("Mayores de edad: " + mayoresDeEdad);
+            Console.WriteLine("Menores de edad: " + menoresDeEdad);
+
+        }
+    }
+}
---
+++ Guia While - Do while #3/Ejercicio 9.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 1000000;
+            int minNumero = 1000000;
+
+            do
+            {
+                if (numero < minNumero)
+                {
+                    minNumero = numero;
+                }
+                Console.Write("Ingrese un numero entero positivo: ");
+                numero = Convert.ToInt32(Console.ReadLine());
+            }
+            while (numero >= 0) ;
+
+            Console.WriteLine("");
+            Console.WriteLine("Numero mas chico: " + minNumero);
+
+        }
+    }
+}
