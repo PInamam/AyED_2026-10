dc053d874c344e8325128da5f6efea459a31b966
PInamam <pablomamani200989@gmail.com>
2026-08-25 13:59:28 -0300

    Add files via upload

---
+++ Guia N°1 - Arrays/Ejercicio 10 Censo de edades del barrio.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] edades = new int[12];
+            int menoresDeEdad = 0;
+            int adultosMayores = 0;
+            int rangoIntermedio = 0;
+
+            for (int i = 0; i < edades.Length; i++)
+            {
+                do
+                {
+                    Console.Write("Edad de la persona " + (i + 1) + ": ");
+                    edades[i] = Convert.ToInt32(Console.ReadLine());
+                    if (edades[i] < 0)
+                    {
+                        Console.WriteLine("Edad no valida");
+                    }
+                    else
+                    {
+                        if (edades[i] < 18)
+                        {
+                            menoresDeEdad++;
+                        }
+                        else if (edades[i] < 65)
+                        {
+                            rangoIntermedio++;
+                        }
+                        else
+                        {
+                            adultosMayores++;
+                        }
+                    }
+
+                }
+                while (edades[i] < 0);
+            }
+
+            Console.WriteLine("\nMenores de edad(0-17): " + menoresDeEdad);
+            Console.WriteLine("Rango intermedio(18-64): " + rangoIntermedio);
+            Console.WriteLine("Mayores de edad(65+): " + adultosMayores);
+
+        }
+    }
+}
---
+++ Guia N°1 - Arrays/Ejercicio 9 Devolución de producto por posición.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double[] precioProdutos = new double[10];
+            int indiceProductoRemovido;
+            double totalAcumuladoDia = 0;
+            double nuevoTotalAcumulado;
+
+            for (int i = 0; i < precioProdutos.Length; i++)
+            {
+                do
+                {
+                    Console.Write("Precio del producto n°" + (i + 1) + ": $");
+                    precioProdutos[i] = Convert.ToDouble(Console.ReadLine());
+                    if (precioProdutos[i] < 0)
+                    {
+                        Console.WriteLine("Ingrese un numero positivo");
+                    }
+                    else
+                    {
+                        totalAcumuladoDia += precioProdutos[i];
+                    }
+                }
+                while (precioProdutos[i] < 0);
+            }
+
+            Console.WriteLine("\nTotal acumulado del dia: $" + totalAcumuladoDia);
+
+            do
+            {
+                Console.Write("\nIngrese la posicion del producto a descontar(1-10): ");
+                indiceProductoRemovido = Convert.ToInt32(Console.ReadLine()) - 1;
+                if (indiceProductoRemovido < 0 || indiceProductoRemovido > (precioProdutos.Length - 1))
+                {
+                    Console.WriteLine("Posicion no valida");
+                }
+            }
+            while (indiceProductoRemovido < 0 || indiceProductoRemovido > (precioProdutos.Length - 1));
+
+            nuevoTotalAcumulado = totalAcumuladoDia - precioProdutos[indiceProductoRemovido];
+
+            Console.WriteLine("\nPrecio del producto removido: $" + precioProdutos[indiceProductoRemovido]);
+            Console.WriteLine("\nNuevo total acumulado: $" + nuevoTotalAcumulado);
+
+        }
+    }
+}
