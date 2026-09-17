b6746a08c5d18ac797d243292a19d2829af64622
PInamam <pablomamani200989@gmail.com>
2026-09-17 16:55:17 -0300

    Add files via upload

---
+++ Funciones 2/Ejercicio5.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Ingrese un numero entero positivo: ");
+            int numeroIngresado = Convert.ToInt32(Console.ReadLine());
+
+            int auxiliar = numeroIngresado;
+            int numeroInv = 0;
+            int ultimoDigito = 0;
+            int contador = 0;
+
+            while(auxiliar > 0)
+            {
+                ultimoDigito = auxiliar % 10;
+                numeroInv = (numeroInv * 10) + ultimoDigito;
+                contador++;
+                auxiliar = auxiliar / 10;
+            }
+
+            Console.WriteLine("Cantitad de digitos: " + contador);
+            if (numeroIngresado == numeroInv)
+            {
+                Console.WriteLine("El numero " + numeroIngresado + " si es capicua");
+            }
+            else
+            {
+                Console.WriteLine("El numero " + numeroIngresado + " no es capicua");
+            }
+
+
+        }
+
+
+    }
+}
