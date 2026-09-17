88eaf2ba38a66928be9cc39b557387a06636fbf2
PInamam <pablomamani200989@gmail.com>
2026-09-17 15:21:17 -0300

    Add files via upload

---
+++ Funciones 2/Ejercicio 10.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Ingrese un numero entero positivo: ");
+            int numeroIngresado = Convert.ToInt32(Console.ReadLine());
+            SumaNumerosConsecutivos(numeroIngresado);
+
+        }
+
+        static void SumaNumerosConsecutivos(int numero)
+        {
+            bool esValido = false;
+            for(int i = 1; i <= numero/2; i++)
+            {
+                int acumulador = 0;
+                for (int j = i; acumulador < numero; j++)
+                {
+                    acumulador = acumulador + j;
+                    if (acumulador == numero)
+                    {
+                        MostrarSuma(numero, i);
+                        esValido = true;
+                    }
+                }
+            }
+            if (esValido == false)
+            {
+                Console.WriteLine("El numero " + numero + " no se puede representar como la suma de numeros consecutivos");
+            }
+        }
+    
+        static void MostrarSuma(int numero, int inicio)
+        {
+            int contador = inicio;
+            Console.Write(numero + " = " + inicio);
+            for (int i = inicio + 1; contador < numero; i++)
+            {
+                contador += i;
+                Console.Write(" + " + i);
+            }
+            Console.WriteLine("");
+        }
+
+
+    }
+}
---
+++ Funciones 2/Ejercicio 9.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("¿Cuantos numeros enteros desea ingresar?: ");
+            int cantidadNumerosIngresados = Convert.ToInt32(Console.ReadLine());
+
+            int[] numeros = new int[cantidadNumerosIngresados];
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                Console.Write("Ingrese el numero N°" + (i + 1) + ": ");
+                numeros[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int maxApariciones = MaxApariciones(numeros);
+            NumeroConMasApariciones(numeros, maxApariciones);
+        }
+
+
+        static int MaxApariciones(int[] numeros)
+        {
+            int maxApariciones = 0;
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                int apariciones = 0;
+                for (int j = 0; j < numeros.Length; j++)
+                {
+                    if (numeros[i] == numeros[j])
+                    {
+                        apariciones++;
+                    }
+                }
+                if (apariciones >= maxApariciones)
+                {
+                    maxApariciones = apariciones;
+                }
+            }
+
+            return maxApariciones;
+        }
+
+        static void NumeroConMasApariciones(int[] numeros, int apariciones)
+        {
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                bool primeraAparicion = true;
+                int cont = 0;
+
+                for (int k = 0; k < i; k++)
+                {
+                    if (numeros[k] == numeros[i])
+                    {
+                        primeraAparicion = false;
+                    }
+                }
+
+                if (primeraAparicion == true)
+                {
+                    for (int j = 0; j < numeros.Length; j++)
+                    {
+                        if (numeros[i] == numeros[j])
+                        {
+                            cont++;
+                            if (cont == apariciones)
+                            {
+                                Console.WriteLine("El numero " + (numeros[i]) + " aparece " + apariciones + " veces");
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+
+
+    }
+}
